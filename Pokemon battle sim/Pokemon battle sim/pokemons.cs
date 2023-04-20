ce59cec8e607380b2a72a3def66e6ebe17317327
Matthew <99067338@mydavinci.nl>
2023-04-20 14:40:21 +0200

    Assignment 1 klaar

--- Pokemon battle sim/Pokemon battle sim/pokemons.cs
+++ Pokemon battle sim/Pokemon battle sim/pokemons.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+
+namespace Pokemons
+{ 
+    class Pokemon
+    {
+
+        private string name;
+        private string weakness;
+        private string strength;
+
+        public Pokemon (string name, string weakness, string strength)
+        {
+            this.name = name;
+            this.weakness = weakness; 
+            this.strength = strength;
+
+        }
+
+        public void battlecry(string name)
+        {
+            
+          Console.WriteLine("" + name + " ");
+            
+           
+        }
+
+
+    }
+}
--- Pokemon battle sim/Pokemon battle sim/program.cs
+++ Pokemon battle sim/Pokemon battle sim/program.cs
@@ -3,10 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Pokemons;
 
 namespace Pokemon_battle_sim
 {
-    internal class progan
+    internal class program
     {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("You caught a wild charmander!");
+            Console.WriteLine("How do u want to name it?");
+            string name = "" + Console.ReadLine();
+            Pokemon charmender = new Pokemon(name, "fire", "water");
+            for (int o = 0; o < 2;)
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    charmender.battlecry(name);
+                }
+                Console.WriteLine("Do u want to rename it?");
+                string antwoord = "" + Console.ReadLine();
+                if (antwoord == "yes")
+                {
+                    Console.WriteLine("'What do u want to name it");
+                    name = "" + Console.ReadLine();
+                }
+                else { break; }
+                
+            }
+        }
     }
 }
